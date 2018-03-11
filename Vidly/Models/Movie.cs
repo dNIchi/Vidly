c2721520d4f367471140abf5f83c3cb39436af9f
dnichi <mmeza@programmer.net>
2018-03-11 04:05:42 -0400

    created project, added PowerTools, Web Essentials, updated Resharper. Added Movies POCO, controller modified Index -> Random. Added Random View (hardcoded model in controller) tested application.

---
+++ Vidly/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    //rep state && behavior of application in terms of its problem domain
+    //ie /movies/random
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
